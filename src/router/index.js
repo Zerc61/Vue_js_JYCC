028a0ce9e41753fac632d6747728edeec1c122ab
Zerc61 <sampbernad@gmail.com>
2025-11-19 16:43:14 +0700

    perbaikan\

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 import login from "@/pages/login.vue";
 import registerView from "@/pages/register.vue";
@@ -18,35 +18,12 @@ import HargaEmasView from "@/pages/HargaEmasView.vue";
 const routes = [
   { path: "/", redirect: "/login" },
 
-<<<<<<< HEAD
+  // User routes
   { path: "/login", name: "Login", component: login },
   { path: "/register", name: "Register", component: registerView },
-=======
-    { path: '/login', component: login },
-    { path: '/register', component: registerView },
-    { path: '/dashboard', component: dashboardView },
-    { path: '/topup', component: topupView },
-    { path: '/isisaldo', component: isisaldoView },
-    { path: '/konfirmasi', component: konfirmasiView },
-
-    // Admin Pages
-    { path: '/admin', component: adminDashboard },
-    { path: '/admin/users', component: AdminUser },
-
-    
-    // NEW: Harga Emas
-    { path: "/harga-emas", component: HargaEmasView },
-
-
-    { path: '/login', name: 'Login', component: login },
-    { path: '/register', name: 'Register', component: registerView },
->>>>>>> 3cc69872caa383faa299858296082c8f2ac33430
-
   { path: "/dashboard", name: "Dashboard", component: dashboardView },
-
   { path: "/topup", name: "Topup", component: topupView },
   { path: "/isisaldo", name: "IsiSaldo", component: isisaldoView },
-
   {
     path: "/konfirmasi",
     name: "Konfirmasi",
@@ -57,25 +34,20 @@ const routes = [
       dcoin: route.params.dcoin,
     }),
   },
-
-<<<<<<< HEAD
   { path: "/pembayaran", name: "Pembayaran", component: PembayaranView },
 
   // Admin routes
   { path: "/admin", name: "AdminDashboard", component: adminDashboard },
   { path: "/admin/users", name: "AdminUser", component: AdminUser },
   { path: "/admin/umkms", name: "AdminUmkm", component: AdminUmkm },
-=======
-    // Admin routes
-    { path: '/admin', name: 'AdminDashboard', component: adminDashboard },
-    { path: '/admin/users', name: 'AdminUser', component: AdminUser },
 
->>>>>>> 3cc69872caa383faa299858296082c8f2ac33430
+  // Other
+  { path: "/harga-emas", component: HargaEmasView },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
